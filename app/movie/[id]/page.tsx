b44fb44914942611eac97193733ef95b71f3414b
mertcetiin <mrtctn506@gmail.com>
2023-11-28 15:06:34 +0300

    error and loaind added

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -4,7 +4,20 @@ import MovieContainer from '@/containers/movie'
 import Movies from '@/mocks/movies.json'
 import { notFound } from 'next/navigation'
 
-function MoviePage({ params }: any) {
+interface MoviePageProps {
+    params: {
+        id: string;
+    };
+    searchParams: {
+        error: string
+    }
+}
+
+async function delay(ms: any) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+function MoviePage({ params, searchParams }: MoviePageProps) {
 
     const movieDetail = Movies.results.find(movie => movie.id.toString() === params.id)
 
@@ -12,9 +25,13 @@ function MoviePage({ params }: any) {
         notFound()
     }
 
+    if (searchParams.error === 'true') {
+        throw new Error("Error happened");
+    }
+
     return (
         <MovieContainer movie={movieDetail} />
     )
 }
 
-export default MoviePage
+export default MoviePage;
